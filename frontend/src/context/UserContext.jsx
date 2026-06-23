4ee552d91ec9bcd0ceb5cf1851536eaccc3f84ce
bargali-dev <bargali.dev09@gmail.com>
2026-06-23 11:57:40 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { io } from "socket.io-client";
 
-export let socket = io("https://linkedin-backend-tpca.onrender.com");
+export let socket = io("https://backend-linkedin-pe2v.onrender.com");
 
 export const userDataContext = createContext();
 const UserContext = ({children}) => {
